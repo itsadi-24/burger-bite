38d8b5dbd66fe1e34799053d989e486869a48fea
itsadi-24 <adipkay@gmail.com>
2024-03-01 20:10:03 +0530

    comment modified

--- app/components/Reservation/Reservation.tsx
+++ app/components/Reservation/Reservation.tsx
@@ -5,7 +5,7 @@ import ReservationForm from './ReservationForm';
 const Reservation = () => {
   return (
     <div className=' pt-[5rem] bg-center bg-cover relative mt-[2rem] pb-[5rem] mb-[3rem] bg-[url("/images/bg-black.jpg")] '>
-      // overlay
+      {/* overlay */}
       <div className=' absolute top-0 left-0 w-full h-full bg-[#000000e0] right-0 bottom-0'></div>
       <div className=' w-[80%] relative z-[20] mx-auto grid grid-cols-1 lg:grid-cols-2 items-center gap-[3rem]'>
         {/* Text content  */}
